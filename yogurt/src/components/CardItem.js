39608d12ddd4182c2b0b4360e2e2854f18edcda2
David S <dshi2008mayo@gmail.com>
2025-07-10 16:54:03 -0400

    shenanigans with links + food!
    - figured out that Link doesnt support opening links in a new tab so had to change CardItem logic a bit
    - added a bunch of restaraunts still adding more

--- yogurt/src/components/CardItem.js
+++ yogurt/src/components/CardItem.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 function CardItem(props) {
+
+ const isExternal = props.path.startsWith('http');
   return (
     <>
       <li className='cards-item'>
+        {isExternal ? (
+            <a href={props.path} 
+                className='cards-item-link' 
+                target="_blank"
+                rel="noopener noreferrer">
+                <figure className='cards-item-pic-wrap' data-category={props.label}> 
+                    <img src={props.src} alt="4Jessie" className='cards-item-img'/>
+                </figure>
+            <div className="cards-item-info">
+                <h5 className="cards-item-text">
+                    {props.text}
+                </h5>
+            </div>
+            </a>
+        ) : (
         <Link className='cards-item-link' to={props.path}>
          <figure className='cards-item-pic-wrap' data-category={props.label}> 
             <img src={props.src} alt="4Jessie" className='cards-item-img'/>
@@ -14,6 +31,7 @@ function CardItem(props) {
             </h5>
          </div>
         </Link>
+        )}
       </li>
     </>
   )
--- yogurt/src/components/FoodieCards.js
+++ yogurt/src/components/FoodieCards.js
@@ -5,61 +5,62 @@ import CardItem from './CardItem'
 function FoodieCards() {
   return (
     <div className='cards'>
-      <h1>Foooooooood</h1>
+      <h1> Every place we've eaten at together</h1>
       <div className='cards-container'>
         <div className='cards-wrapper'>
+            <ul className='cards-items'></ul>
             <ul className='cards-items'>
                 <CardItem 
-                src="test_cat.jpeg"
-                text="food test"
-                label="not cat"
-                path='/'
+                src="/foodies/gati_thai_bistro.png"
+                text="Gati Thai Bistro (first date)"
+                label="Andover"
+                path='https://lineardesign.com/wp-content/uploads/2019/06/out-of-business.gif'
                 />
                 <CardItem 
-                src="test_cat.jpeg"
-                text="food test"
-                label="not cat"
-                path='/'
+                src="/foodies/kokoro_udon.webp"
+                text="Kokoro Craft Udon"
+                label="Andover"
+                path='https://kokorocraftudon.com/'
                 />
                 <CardItem 
-                src="test_cat.jpeg"
-                text="food test"
-                label="not cat"
-                path='/'
+                src="/foodies/pazzos_pizza.webp"
+                text="Pazzo Pizza"
+                label="Andover"
+                path='https://lineardesign.com/wp-content/uploads/2019/06/out-of-business.gif'
                 />
             </ul>
             <ul className='cards-items'>
                 <CardItem 
-                src="test_cat.jpeg"
-                text="food test"
-                label="not cat"
-                path='/'
+                src="/foodies/casa_blanca.webp"
+                text="Casa Blanca"
+                label="Andover"
+                path='https://casablancasrestaurant.com/andover-casa-blanca-mexican-restaurant-food-menu'
                 />
                 <CardItem 
-                src="test_cat.jpeg"
-                text="food test"
-                label="not cat"
-                path='/'
+                src="/foodies/otto.webp"
+                text="OTTO"
+                label="Andover"
+                path='https://order.ottoportland.com/order/otto-portland-andover-main-st'
                 />
                 <CardItem 
-                src="test_cat.jpeg"
-                text="food test"
-                label="not cat"
-                path='/'
+                src="/foodies/uburger.webp"
+                text="Uburger"
+                label="Andover"
+                path='https://uburger.com/'
                 />
             </ul>
             <ul className='cards-items'>
                 <CardItem 
-                src="test_cat.jpeg"
-                text="food test"
-                label="not cat"
-                path='/'
+                src="/foodies/brooksy's.webp"
+                text="Brooksy's"
+                label="Andover"
+                path='https://www.brooksyspizza.com/'
                 />
                 <CardItem 
-                src="test_cat.jpeg"
-                text="food test"
-                label="not cat"
-                path='/'
+                src="/foodies/d'agostino's.webp"
+                text="D'agostino's (my fav)"
+                label="Andover"
+                path='https://dagsdeli.com/'
                 />
                 <CardItem 
                 src="test_cat.jpeg"
